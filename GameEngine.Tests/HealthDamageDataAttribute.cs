fda020f81efe7236f673d5b9947976b2c6ff190f
luciano.f.de.brito <luciano.f.de.brito@avanade.com>
2023-08-18 15:19:01 -0300

    Add comment to HealthDamageDataAttribute

--- GameEngine.Tests/HealthDamageDataAttribute.cs
+++ GameEngine.Tests/HealthDamageDataAttribute.cs
@@ -8,6 +8,8 @@ namespace GameEngine.Tests
     {
         public override IEnumerable<object[]> GetData(MethodInfo testMethod)
         {
+            // Could get data to same way implemented to class ExternalHealthDamageTestdata
+
             yield return new object[] { 0, 100 };
             yield return new object[] { 1, 99 };
             yield return new object[] { 50, 50 };
